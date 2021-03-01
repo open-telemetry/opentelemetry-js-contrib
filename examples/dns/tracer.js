ad9379a67c8ecfd4d7e0dfc1fdba970d95af29e3
Gerhard Stöbich <deb2001-github@yahoo.de>
2021-03-01 19:51:53 +0100

    chore: use instrumentation-dns in example (#358)

--- examples/dns/tracer.js
+++ examples/dns/tracer.js
@@ -3,22 +3,14 @@
 const opentelemetry = require('@opentelemetry/api');
 const { NodeTracerProvider } = require('@opentelemetry/node');
 const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
+const { DnsInstrumentation } = require('@opentelemetry/instrumentation-dns');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
 
 const EXPORTER = process.env.EXPORTER || '';
 
 module.exports = (serviceName) => {
-  const provider = new NodeTracerProvider({
-    plugins: {
-      dns: {
-        enabled: true,
-        path: '@opentelemetry/plugin-dns',
-        // Avoid dns lookup loop with http zipkin calls
-        ignoreHostnames: ['localhost'],
-      },
-    },
-  });
+  const provider = new NodeTracerProvider();
 
   let exporter;
   if (EXPORTER.toLowerCase().startsWith('z')) {
@@ -35,6 +27,12 @@ module.exports = (serviceName) => {
 
   // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
   provider.register();
+
+  // eslint-disable-next-line no-new
+  new DnsInstrumentation({
+    // Avoid dns lookup loop with http zipkin calls
+    ignoreHostnames: ['localhost'],
+  });
 
   return opentelemetry.trace.getTracer(serviceName);
 };
