c4dee5c66025054f81b7ad02454b9eb651a928d0
Rauno Viskus <Rauno56@gmail.com>
2021-04-15 10:30:04 +0300

    feat: let the example be broken until the publish for consistency

--- examples/restify/tracer.js
+++ examples/restify/tracer.js
@@ -12,7 +12,7 @@ const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
 
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { RestifyInstrumentation } = require('../../plugins/node/opentelemetry-instrumentation-restify');
+const { RestifyInstrumentation } = require('@opentelemetry/instrumentation-restify');
 
 const Exporter = ((exporterParam) => {
   if (typeof exporterParam === 'string') {
