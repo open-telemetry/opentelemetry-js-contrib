15498cf5892ba26f301e5f35b1249c4ad8eafc04
Daniel Dyla <dyladan@users.noreply.github.com>
2021-06-10 15:07:48 -0400

    chore: api 21 updates

--- plugins/node/opentelemetry-instrumentation-pg/src/instrumentation.ts
+++ plugins/node/opentelemetry-instrumentation-pg/src/instrumentation.ts
@@ -169,7 +169,7 @@ export class PgInstrumentation extends InstrumentationBase {
             );
             // If a parent span exists, bind the callback
             if (parentSpan) {
-              args[args.length - 1] = context.bind(args[args.length - 1]);
+              args[args.length - 1] = context.bind(context.active(), args[args.length - 1]);
             }
           } else if (
             typeof (args[0] as NormalizedQueryConfig).callback === 'function'
@@ -181,7 +181,7 @@ export class PgInstrumentation extends InstrumentationBase {
             );
             // If a parent span existed, bind the callback
             if (parentSpan) {
-              callback = context.bind(callback);
+              callback = context.bind(context.active(), callback);
             }
 
             // Copy the callback instead of writing to args.callback so that we don't modify user's
@@ -250,7 +250,7 @@ export class PgInstrumentation extends InstrumentationBase {
           ) as PgPoolCallback;
           // If a parent span exists, bind the callback
           if (parentSpan) {
-            callback = context.bind(callback);
+            callback = context.bind(context.active(), callback);
           }
         }
 
@@ -263,6 +263,7 @@ export class PgInstrumentation extends InstrumentationBase {
         if (connectResult instanceof Promise) {
           const connectResultPromise = connectResult as Promise<unknown>;
           return context.bind(
+            context.active(),
             connectResultPromise
               .then(result => {
                 // Return a pass-along promise which ends the span and then goes to user's orig resolvers
--- plugins/node/opentelemetry-instrumentation-redis/src/utils.ts
+++ plugins/node/opentelemetry-instrumentation-redis/src/utils.ts
@@ -48,7 +48,7 @@ const endSpan = (span: Span, err?: Error | null) => {
 export const getTracedCreateClient = (tracer: Tracer, original: Function) => {
   return function createClientTrace(this: redisTypes.RedisClient) {
     const client: redisTypes.RedisClient = original.apply(this, arguments);
-    return context.bind(client);
+    return context.bind(context.active(), client);
   };
 };
 
@@ -63,7 +63,7 @@ export const getTracedCreateStreamTrace = (
           return this._patched_redis_stream;
         },
         set(val: EventEmitter) {
-          context.bind(val);
+          context.bind(context.active(), val);
           this._patched_redis_stream = val;
         },
       });
