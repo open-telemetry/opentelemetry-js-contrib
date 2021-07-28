c1282dd5fff71a76935026f885fa1f23da6b5ad2
legendecas <legendecas@gmail.com>
2021-07-28 11:49:55 +0800

    feat(detectors): add alibaba cloud detector

---
+++ detectors/node/opentelemetry-resource-detector-alibaba-cloud/src/detectors/AlibabaCloudEcsDetector.ts
@@ -0,0 +1,141 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  Detector,
+  Resource,
+  ResourceDetectionConfig,
+} from '@opentelemetry/resources';
+import {
+  ResourceAttributes,
+} from '@opentelemetry/semantic-conventions';
+import * as http from 'http';
+import {
+  CloudProviderValues,
+  CloudPlatformValues,
+} from '../semantic-conventions';
+
+/**
+ * The AlibabaCloudEcsDetector can be used to detect if a process is running in
+ * AlibabaCloud ECS and return a {@link Resource} populated with metadata about
+ * the ECS instance. Returns an empty Resource if detection fails.
+ */
+class AlibabaCloudEcsDetector implements Detector {
+  /**
+   * See https://www.alibabacloud.com/help/doc-detail/67254.htm for
+   * documentation about the AlibabaCloud instance identity document.
+   */
+  readonly ALIBABA_CLOUD_IDMS_ENDPOINT = '100.100.100.200';
+  readonly ALIBABA_CLOUD_INSTANCE_IDENTITY_DOCUMENT_PATH =
+    '/latest/dynamic/instance-identity/document';
+  readonly ALIBABA_CLOUD_INSTANCE_HOST_DOCUMENT_PATH = '/latest/meta-data/hostname';
+  readonly MILLISECOND_TIME_OUT = 1000;
+
+  /**
+   * Attempts to connect and obtain an AlibabaCloud instance Identity document.
+   * If the connection is successful it returns a promise containing a
+   * {@link Resource} populated with instance metadata. Returns a promise
+   * containing an empty {@link Resource} if the connection or parsing of the
+   * identity document fails.
+   *
+   * @param config (unused) The resource detection config
+   */
+  async detect(_config?: ResourceDetectionConfig): Promise<Resource> {
+    const {
+      'owner-account-id': accountId,
+      'instance-id': instanceId,
+      'instance-type': instanceType,
+      'region-id': region,
+      'zone-id': availabilityZone,
+    } = await this._fetchIdentity();
+    const hostname = await this._fetchHost();
+
+    return new Resource({
+      [ResourceAttributes.CLOUD_PROVIDER]: CloudProviderValues.ALIBABA_CLOUD,
+      [ResourceAttributes.CLOUD_PLATFORM]: CloudPlatformValues.ALIBABA_CLOUD_ECS,
+      [ResourceAttributes.CLOUD_ACCOUNT_ID]: accountId,
+      [ResourceAttributes.CLOUD_REGION]: region,
+      [ResourceAttributes.CLOUD_AVAILABILITY_ZONE]: availabilityZone,
+      [ResourceAttributes.HOST_ID]: instanceId,
+      [ResourceAttributes.HOST_TYPE]: instanceType,
+      [ResourceAttributes.HOST_NAME]: hostname,
+    });
+  }
+
+  /**
+   * Fetch AlibabaCloud instance document url with http requests. If the
+   * application is running on an ECS instance, we should be able to get back a
+   * valid JSON document. Parses that document and stores the identity
+   * properties in a local map.
+   */
+  private async _fetchIdentity(): Promise<any> {
+    const options = {
+      host: this.ALIBABA_CLOUD_IDMS_ENDPOINT,
+      path: this.ALIBABA_CLOUD_INSTANCE_IDENTITY_DOCUMENT_PATH,
+      method: 'GET',
+      timeout: this.MILLISECOND_TIME_OUT,
+    };
+    const identity = await this._fetchString(options);
+    return JSON.parse(identity);
+  }
+
+  private async _fetchHost(): Promise<string> {
+    const options = {
+      host: this.ALIBABA_CLOUD_IDMS_ENDPOINT,
+      path: this.ALIBABA_CLOUD_INSTANCE_HOST_DOCUMENT_PATH,
+      method: 'GET',
+      timeout: this.MILLISECOND_TIME_OUT,
+    };
+    return await this._fetchString(options);
+  }
+
+  private async _fetchString(options: http.RequestOptions): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const timeoutId = setTimeout(() => {
+        req.abort();
+        reject(new Error('ECS metadata api request timed out.'));
+      }, 1000);
+
+      const req = http.request(options, res => {
+        clearTimeout(timeoutId);
+        const { statusCode } = res;
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', chunk => (rawData += chunk));
+        res.on('end', () => {
+          if (statusCode && statusCode >= 200 && statusCode < 300) {
+            try {
+              resolve(rawData);
+            } catch (e) {
+              reject(e);
+            }
+          } else {
+            reject(
+              new Error('Failed to load page, status code: ' + statusCode)
+            );
+          }
+        });
+      });
+      req.on('error', err => {
+        clearTimeout(timeoutId);
+        reject(err);
+      });
+      req.end();
+    });
+  }
+}
+
+export const alibabaCloudEcsDetector = new AlibabaCloudEcsDetector();
---
+++ detectors/node/opentelemetry-resource-detector-alibaba-cloud/test/detectors/AlibabaCloudEcsDetector.test.ts
@@ -0,0 +1,139 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as nock from 'nock';
+import * as assert from 'assert';
+import { Resource } from '@opentelemetry/resources';
+import { alibabaCloudEcsDetector } from '../../src';
+import { ResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const ALIYUN_HOST = 'http://' + alibabaCloudEcsDetector
+  .ALIBABA_CLOUD_IDMS_ENDPOINT;
+const ALIYUN_IDENTITY_PATH = alibabaCloudEcsDetector
+  .ALIBABA_CLOUD_INSTANCE_IDENTITY_DOCUMENT_PATH;
+const ALIYUN_HOST_PATH = alibabaCloudEcsDetector
+  .ALIBABA_CLOUD_INSTANCE_HOST_DOCUMENT_PATH;
+
+const mockedIdentityResponse = {
+  'image-id': 'my-image-id',
+  'instance-id': 'my-instance-id',
+  'instance-type': 'my-instance-type',
+  'mac': 'my-mac',
+  'owner-account-id': 'my-owner-account-id',
+  'private-ipv4': 'my-private-ipv4',
+  'region-id': 'my-region-id',
+  'serial-number': 'my-serial-number',
+  'zone-id': 'my-zone-id',
+};
+const mockedHostResponse = 'my-hostname';
+
+describe('alibabaCloudEcsDetector', () => {
+  beforeEach(() => {
+    nock.disableNetConnect();
+    nock.cleanAll();
+  });
+
+  afterEach(() => {
+    nock.enableNetConnect();
+  });
+
+  describe('with successful request', () => {
+    it('should return alibaba cloud ecs instance resource', async () => {
+      const scope = nock(ALIYUN_HOST)
+        .persist()
+        .get(ALIYUN_IDENTITY_PATH)
+        .reply(200, () => mockedIdentityResponse)
+        .get(ALIYUN_HOST_PATH)
+        .reply(200, () => mockedHostResponse);
+
+      const resource: Resource = await alibabaCloudEcsDetector.detect();
+
+      scope.done();
+
+      assert.ok(resource);
+
+      assert.deepStrictEqual(resource.attributes, {
+        // cloud resources
+        [ResourceAttributes.CLOUD_PROVIDER]: 'alibaba_cloud',
+        [ResourceAttributes.CLOUD_PLATFORM]: 'alibaba_cloud_ecs',
+        [ResourceAttributes.CLOUD_ACCOUNT_ID]: 'my-owner-account-id',
+        [ResourceAttributes.CLOUD_REGION]: 'my-region-id',
+        [ResourceAttributes.CLOUD_AVAILABILITY_ZONE]: 'my-zone-id',
+
+        // host resources
+        [ResourceAttributes.HOST_ID]: 'my-instance-id',
+        [ResourceAttributes.HOST_TYPE]: 'my-instance-type',
+        [ResourceAttributes.HOST_NAME]: 'my-hostname',
+      });
+    });
+  });
+
+  describe('with unsuccessful request', () => {
+    it('should throw when receiving error response code', async () => {
+      const expectedError = new Error('Failed to load page, status code: 404');
+      const scope = nock(ALIYUN_HOST)
+        .persist()
+        .get(ALIYUN_IDENTITY_PATH)
+        .reply(200, () => mockedIdentityResponse)
+        .get(ALIYUN_HOST_PATH)
+        .reply(404, () => new Error());
+
+      try {
+        await alibabaCloudEcsDetector.detect();
+        assert.ok(false, 'Expected to throw');
+      } catch (err) {
+        assert.deepStrictEqual(err, expectedError);
+      }
+
+      scope.done();
+    });
+
+    it('should throw when timed out', async () => {
+      const expectedError = new Error('ECS metadata api request timed out.');
+      const scope = nock(ALIYUN_HOST)
+        .get(ALIYUN_IDENTITY_PATH)
+        .reply(200, () => mockedIdentityResponse)
+        .get(ALIYUN_HOST_PATH)
+        .delayConnection(2000)
+        .reply(200, () => mockedHostResponse);
+
+      try {
+        await alibabaCloudEcsDetector.detect();
+        assert.ok(false, 'Expected to throw');
+      } catch (err) {
+        assert.deepStrictEqual(err, expectedError);
+      }
+
+      scope.done();
+    });
+
+    it('should throw when replied with an Error', async () => {
+      const expectedError = new Error('NOT FOUND');
+      const scope = nock(ALIYUN_HOST)
+        .get(ALIYUN_IDENTITY_PATH)
+        .replyWithError(expectedError.message);
+
+      try {
+        await alibabaCloudEcsDetector.detect();
+        assert.ok(false, 'Expected to throw');
+      } catch (err) {
+        assert.deepStrictEqual(err, expectedError);
+      }
+
+      scope.done();
+    });
+  });
+});
