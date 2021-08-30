69c146f69da6d2c3973ddadc33fee5b1299c9569
Krystian Kruk <kkruk@sumologic.com>
2021-08-30 17:18:00 +0200

    Fixed error in Safari when collecting LCP metric

--- packages/instrumentation-document-load/src/utils.ts
+++ packages/instrumentation-document-load/src/utils.ts
@@ -69,9 +69,11 @@ export const addSpanPerformancePaintEvents = (span: Span) => {
   if (typeof PerformanceObserver === 'function') {
     const observer = new PerformanceObserver(() => {});
     observer.observe({ type: 'largest-contentful-paint', buffered: true });
-    const [lcpRecord] = observer.takeRecords();
-    if (lcpRecord) {
-      span.addEvent(EventNames.LARGEST_CONTENTFUL_PAINT, lcpRecord.startTime);
+    if (typeof observer.takeRecords === 'function') {
+      const [lcpRecord] = observer.takeRecords();
+      if (lcpRecord) {
+        span.addEvent(EventNames.LARGEST_CONTENTFUL_PAINT, lcpRecord.startTime);
+      }
     }
   }
 };
