a092b621f8f86edbbce5c55a06c6469093c807b8
Jonathan Campos <jonathan@ridealto.com>
2021-09-17 19:23:47 -0500

    fix(opentelemetry/instrumentation-redis) hasOwnProperty check fixes duplicate call check
    in my project this code was called a second time based on importing. However in the case it was called again the check would fail. By switching to hasOwnProperty it fixes this issue.

--- plugins/node/opentelemetry-instrumentation-redis/src/utils.ts
+++ plugins/node/opentelemetry-instrumentation-redis/src/utils.ts
@@ -57,7 +57,7 @@ export const getTracedCreateStreamTrace = (
   original: Function
 ) => {
   return function create_stream_trace(this: redisTypes.RedisClient) {
-    if (!this.stream) {
+    if (!this.hasOwnProperty('stream')) {
       Object.defineProperty(this, 'stream', {
         get() {
           return this._patched_redis_stream;
