78cd4e118e5a41107d84dfd1ae8c4c28e885b27e
Michael Haberman <habmic@users.noreply.github.com>
2021-11-12 09:04:45 +0200

    feat: AWS-SDK SNS Context propagation (#728)

---
+++ plugins/node/opentelemetry-instrumentation-aws-sdk/src/services/sns.ts
@@ -0,0 +1,96 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Span, Tracer, SpanKind } from '@opentelemetry/api';
+import {
+  MessagingDestinationKindValues,
+  SemanticAttributes,
+} from '@opentelemetry/semantic-conventions';
+import {
+  NormalizedRequest,
+  NormalizedResponse,
+  AwsSdkInstrumentationConfig,
+} from '../types';
+import { injectPropagationContext } from './MessageAttributes';
+import { RequestMetadata, ServiceExtension } from './ServiceExtension';
+
+export class SnsServiceExtension implements ServiceExtension {
+  requestPreSpanHook(request: NormalizedRequest): RequestMetadata {
+    let spanKind: SpanKind = SpanKind.CLIENT;
+    let spanName = `SNS ${request.commandName}`;
+    const spanAttributes = {
+      [SemanticAttributes.MESSAGING_SYSTEM]: 'aws.sns',
+    };
+
+    if (request.commandName === 'Publish') {
+      spanKind = SpanKind.PRODUCER;
+
+      spanAttributes[SemanticAttributes.MESSAGING_DESTINATION_KIND] =
+        MessagingDestinationKindValues.TOPIC;
+      const { TopicArn, TargetArn, PhoneNumber } = request.commandInput;
+      spanAttributes[SemanticAttributes.MESSAGING_DESTINATION] =
+        this.extractDestinationName(TopicArn, TargetArn, PhoneNumber);
+
+      spanName = `${spanAttributes[SemanticAttributes.MESSAGING_DESTINATION]} ${
+        request.commandName
+      }`;
+    }
+
+    return {
+      isIncoming: false,
+      spanAttributes,
+      spanKind,
+      spanName,
+    };
+  }
+
+  requestPostSpanHook(request: NormalizedRequest): void {
+    if (request.commandName === 'Publish') {
+      const origMessageAttributes =
+        request.commandInput['MessageAttributes'] ?? {};
+      if (origMessageAttributes) {
+        request.commandInput['MessageAttributes'] = injectPropagationContext(
+          origMessageAttributes
+        );
+      }
+    }
+  }
+
+  responseHook(
+    response: NormalizedResponse,
+    span: Span,
+    tracer: Tracer,
+    config: AwsSdkInstrumentationConfig
+  ): void {}
+
+  extractDestinationName(
+    topicArn: string,
+    targetArn: string,
+    phoneNumber: string
+  ): string {
+    if (topicArn || targetArn) {
+      const arn = topicArn ?? targetArn;
+      try {
+        return arn.substr(arn.lastIndexOf(':') + 1);
+      } catch (err) {
+        return arn;
+      }
+    } else if (phoneNumber) {
+      return phoneNumber;
+    } else {
+      return 'unknown';
+    }
+  }
+}
