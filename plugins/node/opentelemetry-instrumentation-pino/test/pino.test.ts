44beffabb819e693abe44191b6f5422b806935d7
Siim Kallas <siimkallas@gmail.com>
2021-11-15 13:44:37 +0200

    fix: linting issues

--- plugins/node/opentelemetry-instrumentation-pino/test/pino.test.ts
+++ plugins/node/opentelemetry-instrumentation-pino/test/pino.test.ts
@@ -184,7 +184,7 @@ describe('PinoInstrumentation', () => {
 
     describe('binary arguments', () => {
       it('is possible to construct logger with undefined options', () => {
-        logger = pino(undefined as unknown as Pino.LoggerOptions, stream);
+        logger = pino((undefined as unknown) as Pino.LoggerOptions, stream);
         const span = tracer.startSpan('abc');
         context.with(trace.setSpan(context.active(), span), () => {
           testInjection(span);
