534193b6b9dc9af5e2ffcb613b30b8026f793f37
nemo <nemos@cisco.com>
2021-11-28 18:01:52 +0200

    fix(react-load): init logger for react load example

--- examples/react-load/react/src/web-tracer.js
+++ examples/react-load/react/src/web-tracer.js
@@ -3,6 +3,8 @@ import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { BaseOpenTelemetryComponent } from '@opentelemetry/plugin-react-load';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
+const opentelemetry = require('@opentelemetry/api');
+const { diag, DiagConsoleLogger } = opentelemetry;
 
 export default (serviceName) => {
   const provider = new WebTracerProvider();
@@ -19,9 +21,9 @@ export default (serviceName) => {
   });
 
   const tracer = provider.getTracer(serviceName);
-
+  
   BaseOpenTelemetryComponent.setTracer(serviceName)
-  BaseOpenTelemetryComponent.setLogger(provider.logger)
+  diag.setLogger(new DiagConsoleLogger());
 
   return tracer;
 }
