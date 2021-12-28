aa581e08a48d1d7cc3aeeded1a98d0027828b052
Kirill Khaidukov <kkhaidukov@gmail.com>
2021-12-28 19:19:41 +0300

    fix(examples): fix React Load plugin example

--- examples/react-load/react/src/web-tracer.js
+++ examples/react-load/react/src/web-tracer.js
@@ -4,9 +4,15 @@ import { BaseOpenTelemetryComponent } from '@opentelemetry/plugin-react-load';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
 import { diag, DiagConsoleLogger } from '@opentelemetry/api';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 
 export default (serviceName) => {
-  const provider = new WebTracerProvider();
+  const provider = new WebTracerProvider({
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: "react-load-example"
+    }),
+  });
 
   const exporter = new CollectorTraceExporter({
     url: 'http://localhost:55678/v1/trace',
