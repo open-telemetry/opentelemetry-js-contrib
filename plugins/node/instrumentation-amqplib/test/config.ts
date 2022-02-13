abd1c3d93d45480dc32215e434633d6b49aa4389
Amir Blum <amir@aspecto.io>
2022-02-13 17:33:24 +0200

    fix(amqplib): missing file

--- plugins/node/instrumentation-amqplib/test/config.ts
+++ plugins/node/instrumentation-amqplib/test/config.ts
@@ -18,3 +18,4 @@ export const TEST_RABBITMQ_PORT = 22221;
 export const TEST_RABBITMQ_USER = 'username';
 export const TEST_RABBITMQ_PASS = 'password';
 export const rabbitMqUrl = `amqp://${TEST_RABBITMQ_USER}:${TEST_RABBITMQ_PASS}@${TEST_RABBITMQ_HOST}:${TEST_RABBITMQ_PORT}`;
+export const censoredUrl = `amqp://${TEST_RABBITMQ_USER}:***@${TEST_RABBITMQ_HOST}:${TEST_RABBITMQ_PORT}`;
