a22e35d5f091696615cc9812e46be99a77fc09a5
Rauno Viskus <rauno56@gmail.com>
2022-02-21 13:24:52 +0200

    style: remove default config value override

--- plugins/node/instrumentation-fs/src/instrumentation.ts
+++ plugins/node/instrumentation-fs/src/instrumentation.ts
@@ -35,8 +35,8 @@ type FS = typeof fs;
 const hasPromises = parseInt(process.versions.node.split('.')[0], 10) > 8;
 
 export default class FsInstrumentation extends InstrumentationBase<FS> {
-  constructor(protected override _config?: FsInstrumentationConfig) {
-    super('@opentelemetry/instrumentation-fs', VERSION, _config);
+  constructor(config?: FsInstrumentationConfig) {
+    super('@opentelemetry/instrumentation-fs', VERSION, config);
   }
 
   init(): InstrumentationNodeModuleDefinition<FS>[] {
