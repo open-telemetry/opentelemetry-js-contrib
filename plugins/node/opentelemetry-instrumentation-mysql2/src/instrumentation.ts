2229cbe3b503e2b7c54f657e9f29698cc9f8f380
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-02-27 03:37:39 -0800

    chore: use db.system constants from semantic-conventions package (#912)
    * chore: use DbSystemValues constants
    
    * chore: remove unused constants
    
    Co-authored-by: Gerhard Stöbich <deb2001-github@yahoo.de>
    Co-authored-by: Daniel Dyla <dyladan@users.noreply.github.com>
    Co-authored-by: Amir Blum <amir@aspecto.io>

--- plugins/node/opentelemetry-instrumentation-mysql2/src/instrumentation.ts
+++ plugins/node/opentelemetry-instrumentation-mysql2/src/instrumentation.ts
@@ -20,7 +20,10 @@ import {
   InstrumentationNodeModuleDefinition,
   isWrapped,
 } from '@opentelemetry/instrumentation';
-import { SemanticAttributes } from '@opentelemetry/semantic-conventions';
+import {
+  DbSystemValues,
+  SemanticAttributes,
+} from '@opentelemetry/semantic-conventions';
 import type * as mysqlTypes from 'mysql2';
 import { MySQL2InstrumentationConfig } from './types';
 import {
@@ -36,9 +39,8 @@ type formatType = typeof mysqlTypes.format;
 export class MySQL2Instrumentation extends InstrumentationBase<
   typeof mysqlTypes
 > {
-  static readonly COMPONENT = 'mysql';
   static readonly COMMON_ATTRIBUTES = {
-    [SemanticAttributes.DB_SYSTEM]: MySQL2Instrumentation.COMPONENT,
+    [SemanticAttributes.DB_SYSTEM]: DbSystemValues.MYSQL,
   };
 
   constructor(config?: MySQL2InstrumentationConfig) {
