31c4886e168c24496a2c60721eb0d9e3b8732d27
Gerhard Stöbich <deb2001-github@yahoo.de>
2022-03-06 18:47:05 +0100

    fix: avoid leaking winston types (#932)
    The winston instrumentation needs the winston types only internaly.
    Move them into an internal file to avoid leaking it to end user.

--- plugins/node/opentelemetry-instrumentation-winston/src/internal-types.ts
+++ plugins/node/opentelemetry-instrumentation-winston/src/internal-types.ts
@@ -14,12 +14,16 @@
  * limitations under the License.
  */
 
-import { Span } from '@opentelemetry/api';
-import { InstrumentationConfig } from '@opentelemetry/instrumentation';
+import type { Logger as Winston3Logger } from 'winston';
+import type {
+  LoggerInstance as Winston2Logger,
+  LogMethod as Winston2LogMethod,
+} from 'winston2';
+export type Winston3LogMethod = Winston3Logger['write'];
+export type { Winston3Logger };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type LogHookFunction = (span: Span, record: Record<string, any>) => void;
-
-export interface WinstonInstrumentationConfig extends InstrumentationConfig {
-  logHook?: LogHookFunction;
-}
+export type { Winston2LogMethod };
+export type Winston2LoggerModule = {
+  Logger: Winston2Logger & { prototype: { log: Winston2LogMethod } };
+};
+export type { Winston2Logger };
