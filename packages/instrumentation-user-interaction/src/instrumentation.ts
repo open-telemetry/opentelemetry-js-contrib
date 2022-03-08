16aef89a076caf03efefe5f2feb41b91fad6f7e9
Krystian Kruk <kkruk@sumologic.com>
2022-03-08 15:32:03 +0100

    Added 'new.location.href' into navigation spans

--- packages/instrumentation-user-interaction/src/instrumentation.ts
+++ packages/instrumentation-user-interaction/src/instrumentation.ts
@@ -28,6 +28,7 @@ import { PACKAGE_NAME, PACKAGE_VERSION } from './version';
 
 const ZONE_CONTEXT_KEY = 'OT_ZONE_CONTEXT';
 const EVENT_NAVIGATION_NAME = 'Navigation:';
+const NEW_LOCATION_HREF = 'new.location.href';
 const DEFAULT_EVENT_NAMES: EventName[] = ['click'];
 
 function defaultShouldPreventSpanCreation() {
@@ -408,7 +409,7 @@ export class UserInteractionInstrumentation extends InstrumentationBase<UserInte
         const result = original.apply(this, args);
         const urlAfter = getCurrentLocation();
         if (url !== urlAfter) {
-          plugin._updateInteractionName(urlAfter);
+          plugin._updateSpanAsNavigation(api.trace.getSpan(api.context.active()));
         }
         return result;
       };
@@ -419,10 +420,10 @@ export class UserInteractionInstrumentation extends InstrumentationBase<UserInte
    * Updates interaction span name
    * @param url
    */
-  _updateInteractionName(url: string) {
-    const span: api.Span | undefined = api.trace.getSpan(api.context.active());
+  _updateSpanAsNavigation(span?: api.Span) {
     if (span && typeof span.updateName === 'function') {
-      span.updateName(`${EVENT_NAVIGATION_NAME} ${url}`);
+      span.updateName(`${EVENT_NAVIGATION_NAME} ${getCurrentLocation()}`);
+      span.setAttribute(NEW_LOCATION_HREF, location.href);
     }
   }
 
@@ -591,12 +592,9 @@ export class UserInteractionInstrumentation extends InstrumentationBase<UserInte
       'zone:',
       !!ZoneWithPrototype
     );
-    const that = this;
 
     this.__hashChangeHandler = () => {
-      if (that.lastCreatedSpan && typeof that.lastCreatedSpan.updateName === 'function') {
-        that.lastCreatedSpan.updateName(`${EVENT_NAVIGATION_NAME} ${getCurrentLocation()}`);
-      }
+      this._updateSpanAsNavigation(this.lastCreatedSpan);
     };
 
     window.addEventListener('hashchange', this.__hashChangeHandler);
