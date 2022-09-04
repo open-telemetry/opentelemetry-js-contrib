ad893340d96829d8bdc57a561956df01da269188
abhinav mathur <abhinama@cisco.com>
2022-09-03 22:24:21 -0400

    added another block to test for v2

--- detectors/node/opentelemetry-resource-detector-docker/src/detectors/DockerCGroupV1Detector.ts
+++ detectors/node/opentelemetry-resource-detector-docker/src/detectors/DockerCGroupV1Detector.ts
@@ -25,9 +25,10 @@ import * as fs from 'fs';
 import * as util from 'util';
 import { diag } from '@opentelemetry/api';
 
-export class DockerCGroupV1Detector implements Detector {
+export class DockerDetector implements Detector {
   readonly CONTAINER_ID_LENGTH = 64;
   readonly DEFAULT_CGROUP_PATH = '/proc/self/cgroup';
+  readonly DEFAULT_CGROUP_V2_PATH = '/proc/self/mountinfo';
   readonly UTF8_UNICODE = 'utf8';
 
   private static readFileAsync = util.promisify(fs.readFile);
@@ -42,7 +43,7 @@ export class DockerCGroupV1Detector implements Detector {
           });
     } catch (e) {
       diag.info(
-        'Docker CGROUP V1 Detector did not identify running inside a supported docker container, no docker attributes will be added to resource: ',
+        'Docker Detector did not identify running inside a supported docker container, no docker attributes will be added to resource: ',
         e
       );
       return Resource.empty();
@@ -51,21 +52,34 @@ export class DockerCGroupV1Detector implements Detector {
 
   private async _getContainerId(): Promise<string | undefined> {
     try {
-      const rawData = await DockerCGroupV1Detector.readFileAsync(
+      let rawData = await DockerDetector.readFileAsync(
         this.DEFAULT_CGROUP_PATH,
         this.UTF8_UNICODE
       );
-      const splitData = rawData.trim().split('\n');
+      let splitData = rawData.trim().split('\n');
       for (const str of splitData) {
         if (str.length >= this.CONTAINER_ID_LENGTH) {
           return str.substring(str.length - this.CONTAINER_ID_LENGTH);
         }
       }
+      // If this code is reached - we then check for the V2_PATH
+      rawData = await DockerDetector.readFileAsync(
+        this.DEFAULT_CGROUP_V2_PATH,
+        this.UTF8_UNICODE
+      );
+
+      splitData = rawData.trim().split('\n');
+      for (const str of splitData) {
+        if (str.length >= this.CONTAINER_ID_LENGTH) {
+          return str.substring(str.length - this.CONTAINER_ID_LENGTH);
+        }
+      }
+
     } catch (e) {
       if (e instanceof Error) {
         const errorMessage = e.message;
         diag.info(
-          'Docker CGROUP V1 Detector failed to read the Container ID: ',
+          'Docker Detector failed to read the Container ID: ',
           errorMessage
         );
       }
@@ -74,4 +88,4 @@ export class DockerCGroupV1Detector implements Detector {
   }
 }
 
-export const dockerCGroupV1Detector = new DockerCGroupV1Detector();
+export const dockerDetector = new DockerDetector();
