c109ab39e829c86156430ec9888a02b7712e9d81
Henri Normak <henri.normak@gmail.com>
2022-09-26 10:26:13 +0300

    fix: add missing export
    Also make instrumentation export explicit

--- plugins/node/instrumentation-dataloader/src/index.ts
+++ plugins/node/instrumentation-dataloader/src/index.ts
@@ -14,4 +14,5 @@
  * limitations under the License.
  */
 
-export * from './instrumentation';
+export { DataloaderInstrumentationConfig } from './types';
+export { DataloaderInstrumentation } from './instrumentation';
