b2494929c131ef43929c7923b179eba2436bc2f1
Daniel Dyla <dyladan@users.noreply.github.com>
2022-09-28 08:22:28 -0400

    fix(express): use the same clock for span start and end

--- plugins/node/opentelemetry-instrumentation-express/src/instrumentation.ts
+++ plugins/node/opentelemetry-instrumentation-express/src/instrumentation.ts
@@ -263,22 +263,19 @@ export class ExpressInstrumentation extends InstrumentationBase<
           );
         }
 
-        const startTime = hrTime();
         let spanHasEnded = false;
-        // If we found anything that isnt a middleware, there no point of measuring
-        // their time since they dont have callback.
         if (
           metadata.attributes[AttributeNames.EXPRESS_TYPE] !==
           ExpressLayerType.MIDDLEWARE
         ) {
-          span.end(startTime);
+          span.end();
           spanHasEnded = true;
         }
         // listener for response.on('finish')
         const onResponseFinish = () => {
           if (spanHasEnded === false) {
             spanHasEnded = true;
-            span.end(startTime);
+            span.end();
           }
         };
         // verify we have a callback
