668cb77539078d4959e8d690fc8c382e72e02ed7
haddasb <haddasb@cisco.com>
2022-11-23 12:40:40 +0200

    fix(redis): add tests

--- plugins/node/opentelemetry-instrumentation-ioredis/test/ioredis.test.ts
+++ plugins/node/opentelemetry-instrumentation-ioredis/test/ioredis.test.ts
@@ -1002,41 +1002,4 @@ describe('ioredis', () => {
       });
     });
   });
-
-  describe('#defaultDbStatementSerializer()', () => {
-    [
-      {
-        cmdName: 'UNKNOWN',
-        cmdArgs: ['something'],
-        expected: 'UNKNOWN [1 other arguments]',
-      },
-      {
-        cmdName: 'ECHO',
-        cmdArgs: ['echo'],
-        expected: 'ECHO [1 other arguments]',
-      },
-      {
-        cmdName: 'LPUSH',
-        cmdArgs: ['list', 'value'],
-        expected: 'LPUSH list [1 other arguments]',
-      },
-      {
-        cmdName: 'HSET',
-        cmdArgs: ['hash', 'field', 'value'],
-        expected: 'HSET hash field [1 other arguments]',
-      },
-      {
-        cmdName: 'INCRBY',
-        cmdArgs: ['key', 5],
-        expected: 'INCRBY key 5',
-      },
-    ].forEach(({ cmdName, cmdArgs, expected }) => {
-      it(`should serialize the correct number of arguments for ${cmdName}`, () => {
-        assert.strictEqual(
-          defaultDbStatementSerializer(cmdName, cmdArgs),
-          expected
-        );
-      });
-    });
-  });
 });
