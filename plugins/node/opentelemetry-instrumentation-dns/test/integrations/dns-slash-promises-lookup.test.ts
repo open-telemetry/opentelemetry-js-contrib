9cbcdb7baffc1dbf865aeb7569540a41efebd20e
mhassan1 <marc.j.hassan@gmail.com>
2023-02-07 11:21:23 -0500

    chore(instrumentation-dns): fix ts error

--- plugins/node/opentelemetry-instrumentation-dns/test/integrations/dns-slash-promises-lookup.test.ts
+++ plugins/node/opentelemetry-instrumentation-dns/test/integrations/dns-slash-promises-lookup.test.ts
@@ -105,7 +105,7 @@ describe('dns/promises.lookup()', () => {
         try {
           await dnsPromises.lookup(hostname);
           assert.fail();
-        } catch (error) {
+        } catch (error: any) {
           const spans = memoryExporter.getFinishedSpans();
           const [span] = spans;
 
@@ -127,7 +127,7 @@ describe('dns/promises.lookup()', () => {
       try {
         await dnsPromises.lookup(hostname, { family: -1 });
         assert.fail();
-      } catch (error) {
+      } catch (error: any) {
         const spans = memoryExporter.getFinishedSpans();
         const [span] = spans;
 
@@ -149,7 +149,7 @@ describe('dns/promises.lookup()', () => {
       try {
         await dnsPromises.lookup(hostname as any, { family: 4 });
         assert.fail();
-      } catch (error) {
+      } catch (error: any) {
         const spans = memoryExporter.getFinishedSpans();
         const [span] = spans;
 
