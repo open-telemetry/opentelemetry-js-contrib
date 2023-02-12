bde31ac641689efd34cc5da19d3dbcb96b09694a
Jakub Koralewski <contact@jcubed.me>
2023-02-12 12:50:50 +0000

    fix(instrumentation-fs): lint (typeof fs)

--- plugins/node/instrumentation-fs/src/types.ts
+++ plugins/node/instrumentation-fs/src/types.ts
@@ -44,8 +44,8 @@ export type FMember =
   | FunctionPropertyNames<typeof fs>
   | FunctionPropertyNamesTwoLevels<typeof fs>;
 export type FPMember =
-  | FunctionPropertyNames<typeof fs['promises']>
-  | FunctionPropertyNamesTwoLevels<typeof fs['promises']>;
+  | FunctionPropertyNames<(typeof fs)['promises']>
+  | FunctionPropertyNamesTwoLevels<(typeof fs)['promises']>;
 
 export type CreateHook = (
   functionName: FMember | FPMember,
