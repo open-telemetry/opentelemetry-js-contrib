dd875ff3a71df8493a3cb0ce2446453a0c3f375b
haddasbronfman <haddasb@cisco.com>
2023-02-14 16:07:36 +0200

    fix(postgres): lint

--- plugins/node/opentelemetry-instrumentation-pg/test/pg.test.ts
+++ plugins/node/opentelemetry-instrumentation-pg/test/pg.test.ts
@@ -260,7 +260,7 @@ describe('pg', () => {
     });
 
     it('should pass the client connection object in the callback function', done => {
-      connClient.connect(function (err: Error, conn? : pg.Client) {
+      connClient.connect(function (err: Error, conn?: pg.Client) {
         //the 'connect' function passes the client object as an argument
         //in the callback function: https://github.com/brianc/node-postgres/blob/master/packages/pg/lib/client.js#L282
         assert(arguments[1] === connClient);
