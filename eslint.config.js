6d8f632a27c7ce9ed3d8ddfc562db478609fb63b
Sami Musallam <samusall@cisco.com>
2023-03-02 12:00:12 +0200

    feat(auto-instrumentation-agent): updated eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,11 @@ module.exports = {
     "import"
   ],
   extends: [
-      "./node_modules/gts",
+    "./node_modules/gts",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-      "project": "./tsconfig.json"
+    "project": "./tsconfig.json"
   },
   rules: {
     "quotes": [2, "single", { "avoidEscape": true }],
@@ -17,21 +17,21 @@ module.exports = {
     "eqeqeq": "off",
     "prefer-rest-params": "off",
     "@typescript-eslint/naming-convention": [
-        "error",
-        {
-          "selector": "memberLike",
-          "modifiers": ["private", "protected"],
-          "format": ["camelCase"],
-          "leadingUnderscore": "require"
-        }
+      "error",
+      {
+        "selector": "memberLike",
+        "modifiers": ["private", "protected"],
+        "format": ["camelCase"],
+        "leadingUnderscore": "require"
+      }
     ],
     "@typescript-eslint/no-inferrable-types": ["error", { ignoreProperties: true }],
     "arrow-parens": ["error", "as-needed"],
     "prettier/prettier": ["error", { "singleQuote": true, "arrowParens": "avoid" }],
     "node/no-deprecated-api": ["warn"],
     "header/header": [2, "block", [{
-        pattern: / \* Copyright The OpenTelemetry Authors[\r\n]+ \*[\r\n]+ \* Licensed under the Apache License, Version 2\.0 \(the \"License\"\);[\r\n]+ \* you may not use this file except in compliance with the License\.[\r\n]+ \* You may obtain a copy of the License at[\r\n]+ \*[\r\n]+ \* {6}https:\/\/www\.apache\.org\/licenses\/LICENSE-2\.0[\r\n]+ \*[\r\n]+ \* Unless required by applicable law or agreed to in writing, software[\r\n]+ \* distributed under the License is distributed on an \"AS IS\" BASIS,[\r\n]+ \* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.[\r\n]+ \* See the License for the specific language governing permissions and[\r\n]+ \* limitations under the License\./gm,
-        template:
+      pattern: / \* Copyright The OpenTelemetry Authors[\r\n]+ \*[\r\n]+ \* Licensed under the Apache License, Version 2\.0 \(the \"License\"\);[\r\n]+ \* you may not use this file except in compliance with the License\.[\r\n]+ \* You may obtain a copy of the License at[\r\n]+ \*[\r\n]+ \*      https:\/\/www\.apache\.org\/licenses\/LICENSE-2\.0[\r\n]+ \*[\r\n]+ \* Unless required by applicable law or agreed to in writing, software[\r\n]+ \* distributed under the License is distributed on an \"AS IS\" BASIS,[\r\n]+ \* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.[\r\n]+ \* See the License for the specific language governing permissions and[\r\n]+ \* limitations under the License\./gm,
+      template:
         `\n * Copyright The OpenTelemetry Authors\n *\n * Licensed under the Apache License, Version 2.0 (the "License");\n * you may not use this file except in compliance with the License.\n * You may obtain a copy of the License at\n *\n *      https://www.apache.org/licenses/LICENSE-2.0\n *\n * Unless required by applicable law or agreed to in writing, software\n * distributed under the License is distributed on an "AS IS" BASIS,\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n * See the License for the specific language governing permissions and\n * limitations under the License.\n `
     }]],
     "import/no-extraneous-dependencies": ["error", { devDependencies: ["test/**/*.ts"] }],
