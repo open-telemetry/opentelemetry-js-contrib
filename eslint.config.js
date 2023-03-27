e98178f5bbba2b94adb9678e895f840669cb9a40
Sami Musallam <samusall@cisco.com>
2023-03-27 22:09:14 +0300

    fix(eslint-config): replace gts with prettier and eslint recommended config

--- eslint.config.js
+++ eslint.config.js
@@ -1,52 +1,61 @@
 module.exports = {
-  plugins: [
-    "@typescript-eslint",
-    "header",
-    "import"
-  ],
+  plugins: ['@typescript-eslint', 'header', 'import', 'node', 'prettier'],
   extends: [
-      "./node_modules/gts",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-      "project": "./tsconfig.json"
+    project: './tsconfig.json',
   },
   rules: {
-    "quotes": [2, "single", { "avoidEscape": true }],
-    "@typescript-eslint/no-this-alias": "off",
-    "eqeqeq": "off",
-    "prefer-rest-params": "off",
-    "@typescript-eslint/naming-convention": [
-        "error",
-        {
-          "selector": "memberLike",
-          "modifiers": ["private", "protected"],
-          "format": ["camelCase"],
-          "leadingUnderscore": "require"
-        }
+    quotes: [2, 'single', { avoidEscape: true }],
+    '@typescript-eslint/no-this-alias': 'off',
+    eqeqeq: 'off',
+    'prefer-rest-params': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'memberLike',
+        modifiers: ['private', 'protected'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
     ],
-    "@typescript-eslint/no-inferrable-types": ["error", { ignoreProperties: true }],
-    "arrow-parens": ["error", "as-needed"],
-    "prettier/prettier": ["error", { "singleQuote": true, "arrowParens": "avoid" }],
-    "node/no-deprecated-api": ["warn"],
-    "header/header": [2, "block", [{
-        pattern: / \* Copyright The OpenTelemetry Authors[\r\n]+ \*[\r\n]+ \* Licensed under the Apache License, Version 2\.0 \(the \"License\"\);[\r\n]+ \* you may not use this file except in compliance with the License\.[\r\n]+ \* You may obtain a copy of the License at[\r\n]+ \*[\r\n]+ \*      https:\/\/www\.apache\.org\/licenses\/LICENSE-2\.0[\r\n]+ \*[\r\n]+ \* Unless required by applicable law or agreed to in writing, software[\r\n]+ \* distributed under the License is distributed on an \"AS IS\" BASIS,[\r\n]+ \* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.[\r\n]+ \* See the License for the specific language governing permissions and[\r\n]+ \* limitations under the License\./gm,
-        template:
-        `\n * Copyright The OpenTelemetry Authors\n *\n * Licensed under the Apache License, Version 2.0 (the "License");\n * you may not use this file except in compliance with the License.\n * You may obtain a copy of the License at\n *\n *      https://www.apache.org/licenses/LICENSE-2.0\n *\n * Unless required by applicable law or agreed to in writing, software\n * distributed under the License is distributed on an "AS IS" BASIS,\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n * See the License for the specific language governing permissions and\n * limitations under the License.\n `
-    }]],
-    "import/no-extraneous-dependencies": ["error", { devDependencies: ["test/**/*.ts"] }],
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      { ignoreProperties: true },
+    ],
+    'arrow-parens': ['error', 'as-needed'],
+    'node/no-deprecated-api': ['warn'],
+    'header/header': [
+      2,
+      'block',
+      [
+        {
+          pattern:
+            / \* Copyright The OpenTelemetry Authors[\r\n]+ \*[\r\n]+ \* Licensed under the Apache License, Version 2\.0 \(the \"License\"\);[\r\n]+ \* you may not use this file except in compliance with the License\.[\r\n]+ \* You may obtain a copy of the License at[\r\n]+ \*[\r\n]+ \* {6}https:\/\/www\.apache\.org\/licenses\/LICENSE-2\.0[\r\n]+ \*[\r\n]+ \* Unless required by applicable law or agreed to in writing, software[\r\n]+ \* distributed under the License is distributed on an \"AS IS\" BASIS,[\r\n]+ \* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.[\r\n]+ \* See the License for the specific language governing permissions and[\r\n]+ \* limitations under the License\./gm,
+          template: `\n * Copyright The OpenTelemetry Authors\n *\n * Licensed under the Apache License, Version 2.0 (the "License");\n * you may not use this file except in compliance with the License.\n * You may obtain a copy of the License at\n *\n *      https://www.apache.org/licenses/LICENSE-2.0\n *\n * Unless required by applicable law or agreed to in writing, software\n * distributed under the License is distributed on an "AS IS" BASIS,\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n * See the License for the specific language governing permissions and\n * limitations under the License.\n `,
+        },
+      ],
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['test/**/*.ts'] },
+    ],
   },
   overrides: [
     {
-      "files": ["test/**/*.ts"],
-      "rules": {
-        "no-empty": "off",
-        "@typescript-eslint/ban-ts-ignore": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-var-requires": "off"
-      }
-    }
-  ]
+      files: ['test/**/*.ts'],
+      rules: {
+        'no-empty': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 };
