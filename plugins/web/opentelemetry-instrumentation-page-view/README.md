f95189bf506ad65e1a1ceff04e51d5a14fabcdfc
tomwong2 <tomwong2@cisco.com>
2023-04-26 18:54:23 -0700

    feat: page view instrumentation. init commit

---
+++ plugins/web/opentelemetry-instrumentation-page-view/README.md
@@ -0,0 +1,71 @@
+# OpenTelemetry Page View Event Instrumentation for Web
+
+[![NPM Published Version][npm-img]][npm-url]
+[![Apache License][license-image]][license-image]
+
+**Note: This is an experimental package under active development. New releases may include breaking changes.**
+
+This module provides automatic instrumentation for web pages by sending a log record for a page view event.
+
+## Installation
+
+```bash
+npm install --save @opentelemetry/instrumentation-page-view
+```
+
+## Usage
+
+OpenTelemetry Page View Instrumentation allows the user to automatically collect log data of the page view event and export them to their backend of choice.
+
+To use this instrumentation register it in the list of instrumentations as follows
+
+```js
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { PageViewEventInstrumentation } from "@opentelemetry/instrumentation-page-view";
+import {
+  BatchLogRecordProcessor,
+  LoggerProvider
+} from '@opentelemetry/sdk-logs';
+import { OTLPLogsExporter } from "@opentelemetry/exporter-logs-otlp-proto";
+
+const logProvider = new LoggerProvider();
+
+logProvider.addLogRecordProcessor(
+  new BatchLogRecordProcessor(
+    new OTLPLogsExporter({
+      url: "https://log_endpoint_url",
+      headers: { Accept: "application/x-protobuf" },
+    }),
+    {
+      exportTimeoutMillis: 1000,
+      scheduledDelayMillis: 1000,
+    }
+  ));
+
+registerInstrumentations({
+  instrumentations: [
+      new PageViewEventInstrumentation({ provider: logProvider }),
+  ],
+});
+
+```
+
+## Example
+
+### Page view instrumentation Options
+
+## Useful links
+
+- For more information on OpenTelemetry, visit: <https://opentelemetry.io/>
+- For more about OpenTelemetry JavaScript: <https://github.com/open-telemetry/opentelemetry-js>
+- For help or feedback on this project, join us in [GitHub Discussions][discussions-url]
+
+## License
+
+Apache 2.0 - See [LICENSE][license-url] for more information.
+
+[discussions-url]: https://github.com/open-telemetry/opentelemetry-js/discussions
+[license-url]: https://github.com/open-telemetry/opentelemetry-js/blob/main/LICENSE
+[license-image]: https://img.shields.io/badge/license-Apache_2.0-green.svg?style=flat
+[npm-url]: https://www.npmjs.com/package/@opentelemetry/instrumentation-http
+[npm-img]: https://badge.fury.io/js/%40opentelemetry%2Finstrumentation-http.svg
---
+++ plugins/web/opentelemetry-instrumentation-page-view/src/instrumentation.ts
@@ -0,0 +1,86 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  InstrumentationBase,
+} from '@opentelemetry/instrumentation';
+import { LogRecord, Logger } from '@opentelemetry/api-logs';
+import { VERSION } from './version';
+import {PageViewInstrumentationConfig} from "./types";
+
+/**
+ * This class represents a Page View Event plugin
+ */
+export class PageViewEventInstrumentation extends InstrumentationBase<unknown> {
+  readonly component: string = 'page-view-event';
+  readonly version: string = '1';
+  readonly logger?: Logger;
+
+  /**
+   *
+   * @param config
+   */
+  constructor(config: PageViewInstrumentationConfig = {}) {
+    super('@opentelemetry/instrumentation-page-view', VERSION, config);
+    this.logger = config.provider?.getLogger('page_view');
+  }
+
+  init() {}
+
+  /**
+   * callback to be executed when page is viewed
+   */
+  private _onPageView() {
+    const pageViewEvent: LogRecord = {
+      attributes: {
+        'event.domain': 'browser',
+        'event.name': 'page_view',
+        'event.type': 0,
+        'event.data': {
+          url: document.documentURI as string,
+          referrer: document.referrer,
+          title: document.title,
+        },
+      },
+    };
+
+    this.logger?.emit(pageViewEvent);
+  }
+
+  /**
+   * executes callback {_onPageView } when the DOM Content is loaded
+   */
+  private _waitForPageLoad() {
+    document.addEventListener('DOMContentLoaded', this._onPageView.bind(this));
+  }
+
+  /**
+   * implements enable function
+   */
+  override enable() {
+    // remove previously attached load to avoid adding the same event twice
+    // in case of multiple enable calling.
+    document.removeEventListener('DOMContentLoaded', this._onPageView.bind(this));
+    this._waitForPageLoad();
+  }
+
+  /**
+   * implements disable function
+   */
+  override disable() {
+    document.removeEventListener('DOMContentLoaded', this._onPageView.bind(this));
+  }
+}
