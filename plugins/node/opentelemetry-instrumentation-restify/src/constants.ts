bab09d4e02032c29e594220f088a334bf7f7eacf
Chi Ma <chigia001@gmail.com>
2023-07-12 17:30:42 +0700

    chore(restify): upgrade restify to 11.1.0

--- plugins/node/opentelemetry-instrumentation-restify/src/constants.ts
+++ plugins/node/opentelemetry-instrumentation-restify/src/constants.ts
@@ -24,4 +24,4 @@ export const RESTIFY_METHODS = [
   'patch',
 ];
 export const MODULE_NAME = 'restify';
-export const SUPPORTED_VERSIONS = ['>=4.0.0 <11'];
+export const SUPPORTED_VERSIONS = ['>=4.0.0 <12'];
