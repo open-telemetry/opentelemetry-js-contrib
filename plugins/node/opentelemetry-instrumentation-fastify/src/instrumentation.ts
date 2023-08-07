d052a1bead492d2cae1900d7ddbb13a60d47e740
Francesco Novy <francesco.novy@sentry.io>
2023-08-07 09:24:30 +0200

    fix(fastify): Make sure consturctor patching works with esm

--- plugins/node/opentelemetry-instrumentation-fastify/src/instrumentation.ts
+++ plugins/node/opentelemetry-instrumentation-fastify/src/instrumentation.ts
@@ -198,13 +198,13 @@ export class FastifyInstrumentation extends InstrumentationBase {
   }
 
   private _patchConstructor(
-    original: () => FastifyInstance
-  ): () => FastifyInstance {
+    moduleExports: { fastify: () => FastifyInstance },
+    ): () => FastifyInstance {
     const instrumentation = this;
     this._diag.debug('Patching fastify constructor function');
 
     function fastify(this: FastifyInstance, ...args: any) {
-      const app: FastifyInstance = original.apply(this, args);
+      const app: FastifyInstance = moduleExports.fastify.apply(this, args);
       app.addHook('onRequest', instrumentation._hookOnRequest());
       app.addHook('preHandler', instrumentation._hookPreHandler());
 
