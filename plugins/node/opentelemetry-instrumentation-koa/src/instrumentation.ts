565019dd7d6484fc6cfc1b8c100f0c12bdf8fe78
Paulo Prestes <pprestes@seek.com.au>
2023-09-18 09:52:27 +1000

    fix formatting

--- plugins/node/opentelemetry-instrumentation-koa/src/instrumentation.ts
+++ plugins/node/opentelemetry-instrumentation-koa/src/instrumentation.ts
@@ -23,15 +23,14 @@ import {
 } from '@opentelemetry/instrumentation';
 
 import type * as koa from 'koa';
-import {
-  KoaContext,
-  KoaLayerType,
-  KoaInstrumentationConfig,
-} from './types';
+import { KoaContext, KoaLayerType, KoaInstrumentationConfig } from './types';
 import { VERSION } from './version';
 import { getMiddlewareMetadata, isLayerIgnored } from './utils';
 import { getRPCMetadata, RPCType } from '@opentelemetry/core';
-import { kLayerPatched, KoaMiddleware, KoaPatchedMiddleware } from './internal-types';
+import {
+  kLayerPatched,
+  KoaMiddleware,
+  KoaPatchedMiddleware, } from './internal-types';
 
 /** Koa instrumentation for OpenTelemetry */
 export class KoaInstrumentation extends InstrumentationBase<typeof koa> {
