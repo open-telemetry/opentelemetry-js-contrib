e0db415cc749aa6a2fb77e562ffde65ef7598316
Drew Corlin <drew.corlin@getgarner.com>
2024-01-15 21:46:35 -0500

    Remove commented code

--- packages/esbuild-plugin-node/src/types.ts
+++ packages/esbuild-plugin-node/src/types.ts
@@ -47,28 +47,6 @@ type _RemoveFunctions<T> = {
 // _RemoveFunctions does not work on optional fields, so first make the type required then apply Partial to the result
 export type RemoveFunctions<T> = Partial<_RemoveFunctions<Required<T>>>;
 
-// type BuiltinPackage =
-//   | '@opentelemetry/instrumentation-dns'
-//   | '@opentelemetry/instrumentation-fs'
-//   | '@opentelemetry/instrumentation-http'
-//   | '@opentelemetry/instrumentation-net';
-
-// export type NonBuiltinPackage = Exclude<
-//   keyof InstrumentationConfigMap,
-//   BuiltinPackage
-// >;
-
-// type NonBuiltinInstrumentationConfigMap = Pick<
-//   InstrumentationConfigMap,
-//   NonBuiltinPackage
-// >;
-
-// export type EsbuildInstrumentationConfigMap = {
-//   [K in keyof InstrumentationConfigMap]: RemoveFunctions<
-//     NonBuiltinInstrumentationConfigMap[K]
-//   >;
-// };
-
 type BuiltinPackages =
   | '@opentelemetry/instrumentation-dns'
   | '@opentelemetry/instrumentation-fs'
