0ec47696da1cd19df4113a7a4479d8c305b9aa0f
Marc Pichler <marc.pichler@dynatrace.com>
2024-03-01 11:15:17 +0100

    fix(instrumentation-pg): prevent net.peer.port from being NaN

--- plugins/node/opentelemetry-instrumentation-pg/test/utils.test.ts
+++ plugins/node/opentelemetry-instrumentation-pg/test/utils.test.ts
@@ -28,6 +28,7 @@ import { PgInstrumentationConfig } from '../src';
 import { AttributeNames } from '../src/enums/AttributeNames';
 import { PgClientExtended } from '../src/internal-types';
 import * as utils from '../src/utils';
+import { SemanticAttributes } from "@opentelemetry/semantic-conventions";
 
 const memoryExporter = new InMemorySpanExporter();
 
@@ -193,4 +194,36 @@ describe('utils.ts', () => {
       assert.deepStrictEqual(pgValues, ['0']);
     });
   });
+
+  describe('.getSemanticAttributesFromConnection()', () => {
+    it('should set port attribute to undefined when port is not an integer', () => {
+      assert.strictEqual(utils.getSemanticAttributesFromConnection({
+          port: Infinity,
+        })[SemanticAttributes.NET_PEER_PORT],
+        undefined);
+      assert.strictEqual(utils.getSemanticAttributesFromConnection({
+        port: -Infinity,
+      })[SemanticAttributes.NET_PEER_PORT],
+        undefined);
+      assert.strictEqual(utils.getSemanticAttributesFromConnection({
+          port: NaN,
+        })[SemanticAttributes.NET_PEER_PORT],
+        undefined);
+      assert.strictEqual(utils.getSemanticAttributesFromConnection({
+          port: 1.234,
+        })[SemanticAttributes.NET_PEER_PORT],
+        undefined);
+    });
+
+    it('should set port attribute to undefined when port is an integer', () => {
+      assert.strictEqual(utils.getSemanticAttributesFromConnection({
+          port: 1234,
+        })[SemanticAttributes.NET_PEER_PORT],
+        1234);
+      assert.strictEqual(utils.getSemanticAttributesFromConnection({
+          port: Number.MAX_VALUE,
+        })[SemanticAttributes.NET_PEER_PORT],
+        Number.MAX_VALUE);
+    });
+  })
 });
