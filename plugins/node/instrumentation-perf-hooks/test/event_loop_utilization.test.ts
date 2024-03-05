5a40557618cee84e27414f0e76e73b95a1347e55
Marc Pichler <marc.pichler@dynatrace.com>
2024-03-05 17:16:04 +0100

    fixup! fix(instrumentation-perf-hooks): use TestMetricReader, unref timer

--- plugins/node/instrumentation-perf-hooks/test/event_loop_utilization.test.ts
+++ plugins/node/instrumentation-perf-hooks/test/event_loop_utilization.test.ts
@@ -139,6 +139,6 @@ describe('nodejs.event_loop.utilization', function () {
     );
     const val = metrics[0].dataPoints[0].value;
     assert.strictEqual(val > 0, true, `val (${val}) > 0`);
-    assert.strictEqual(val < 1, true, `val (${val}) < 1`);
+    assert.strictEqual(val <= 1, true, `val (${val}) <= 1`);
   });
 });
