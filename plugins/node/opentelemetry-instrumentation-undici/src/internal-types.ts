9e24242182cd7d3c097b5de5476f7c36ba3fd89a
David Luna <david.luna@elastic.co>
2024-03-05 11:42:24 +0100

    Update plugins/node/opentelemetry-instrumentation-undici/src/internal-types.ts
    Co-authored-by: Jamie Danielson <jamieedanielson@gmail.com>

--- plugins/node/opentelemetry-instrumentation-undici/src/internal-types.ts
+++ plugins/node/opentelemetry-instrumentation-undici/src/internal-types.ts
@@ -15,7 +15,7 @@
  */
 import type { Channel } from 'diagnostics_channel';
 
-import { UndiciRequest, UnidiciResponse } from './types';
+import { UndiciRequest, UndiciResponse } from './types';
 
 export interface ListenerRecord {
   name: string;
