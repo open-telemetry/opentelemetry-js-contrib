44f53e22cf6942d5f70bb00e26c0d884566d6abc
David Luna <luna.bistuer@gmail.com>
2024-03-25 17:43:48 +0100

    chore(instrumentation-undici): add explicitBucketBoundaries

--- plugins/node/instrumentation-undici/src/undici.ts
+++ plugins/node/instrumentation-undici/src/undici.ts
@@ -128,6 +128,9 @@ export class UndiciInstrumentation extends InstrumentationBase {
         description: 'Measures the duration of outbound HTTP requests.',
         unit: 's',
         valueType: ValueType.DOUBLE,
+        advice: {
+          explicitBucketBoundaries: [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
+        }
       }
     );
   }
