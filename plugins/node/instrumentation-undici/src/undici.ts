46226e1bbee4d9586379045fd9d326d8ba7a15f2
David Luna <luna.bistuer@gmail.com>
2024-03-25 17:53:49 +0100

    chore(instrumentation-undici): fix lint errors

--- plugins/node/instrumentation-undici/src/undici.ts
+++ plugins/node/instrumentation-undici/src/undici.ts
@@ -129,8 +129,11 @@ export class UndiciInstrumentation extends InstrumentationBase {
         unit: 's',
         valueType: ValueType.DOUBLE,
         advice: {
-          explicitBucketBoundaries: [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
-        }
+          explicitBucketBoundaries: [
+            0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5,
+            7.5, 10,
+          ],
+        },
       }
     );
   }
