40f630c65936f80c02c9a41dd3503aec40a08cf2
Siim Kallas <siimkallas@gmail.com>
2024-04-24 14:33:15 +0300

    add aspecto to copyright

--- plugins/node/instrumentation-kafkajs/test/kafkajs.test.ts
+++ plugins/node/instrumentation-kafkajs/test/kafkajs.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright The OpenTelemetry Authors
+ * Copyright The OpenTelemetry Authors, Aspecto
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
