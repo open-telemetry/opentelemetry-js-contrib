ce0f46578de5132f947f17faf7db19c10e7dd21e
Jamie Danielson <jamieedanielson@gmail.com>
2024-07-09 19:06:04 -0400

    Update comments

--- plugins/node/opentelemetry-instrumentation-express/test/express.test.ts
+++ plugins/node/opentelemetry-instrumentation-express/test/express.test.ts
@@ -534,8 +534,8 @@ describe('ExpressInstrumentation', () => {
         routerLayer = stack.find(router => router.name === 'CustomRouter');
         return res.status(200).end('bar');
       });
-      // The patched router has now express router's own properties in its prototype so
-      // they are not acessible through `Object.keys(...)`
+      // The patched router now has express router's own properties in its prototype so
+      // they are not accessible through `Object.keys(...)`
       // https://github.com/TryGhost/Ghost/blob/fefb9ec395df8695d06442b6ecd3130dae374d94/ghost/core/core/frontend/web/site.js#L192
       Object.setPrototypeOf(CustomRouter, router);
       expressApp.use(CustomRouter);
