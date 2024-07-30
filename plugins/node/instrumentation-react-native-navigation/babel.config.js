d8830b3d23af073701def977775018c7d0b9e16f
Florencia Acosta <florencia.acosta@embrace.io>
2024-07-30 12:06:53 -0300

    downgrading jsdom version, supporting node 14

--- plugins/node/instrumentation-react-native-navigation/babel.config.js
+++ plugins/node/instrumentation-react-native-navigation/babel.config.js
@@ -2,7 +2,7 @@ module.exports = api => {
   api.cache(false);
 
   return {
-    presets: ['module:metro-react-native-babel-preset', '@babel/preset-env'],
+    presets: ['module:metro-react-native-babel-preset'],
     plugins: ['babel-plugin-react-native-web'],
   };
 };
