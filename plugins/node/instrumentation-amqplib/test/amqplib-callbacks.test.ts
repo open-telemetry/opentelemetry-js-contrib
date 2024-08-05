b54f710644a33fa2609275056ad652cddf404395
Cedric Ziel <cedric.ziel@grafana.com>
2024-08-05 12:09:00 +0200

    Update amqplib-callbacks.test.ts

--- plugins/node/instrumentation-amqplib/test/amqplib-callbacks.test.ts
+++ plugins/node/instrumentation-amqplib/test/amqplib-callbacks.test.ts
@@ -136,13 +136,10 @@ describe('amqplib instrumentation callback model', () => {
         const [publishSpan, consumeSpan] = getTestSpans();
 
         // assert publish span
-        expect(publishSpan.name).toMatch('<default> publish');
+        expect(publishSpan.name).toMatch('publish <default>');
         expect(publishSpan.kind).toEqual(SpanKind.PRODUCER);
         expect(publishSpan.attributes[SEMATTRS_MESSAGING_SYSTEM]).toEqual(
           'rabbitmq'
-        );
-        expect(publishSpan.attributes[SEMATTRS_MESSAGING_OPERATION]).toEqual(
-          'publish'
         );
         expect(publishSpan.attributes[SEMATTRS_MESSAGING_DESTINATION]).toEqual(
           ''
