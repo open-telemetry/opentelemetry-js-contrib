42b7c9958d38635dadc34bc239af8b64996d2086
jjllee <jjllee@amazon.com>
2025-03-05 13:09:41 -0800

    sampler update

--- incubator/opentelemetry-sampler-aws-xray/src/sampling-rule-applier.ts
+++ incubator/opentelemetry-sampler-aws-xray/src/sampling-rule-applier.ts
@@ -13,5 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './remote-sampler';
-export { AWSXRayRemoteSamplerConfig } from './types';
+
+import { ISamplingRule, SamplingTargetDocument } from './types';
+import { SamplingRule } from './sampling-rule';
+import { Statistics } from './statistics';
+
+export class SamplingRuleApplier {
+  public samplingRule: SamplingRule;
+
+  constructor(
+    samplingRule: ISamplingRule,
+    statistics: Statistics = new Statistics(),
+    target?: SamplingTargetDocument
+  ) {
+    this.samplingRule = new SamplingRule(samplingRule);
+  }
+}
