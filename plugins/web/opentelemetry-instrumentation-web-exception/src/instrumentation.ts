4a86d7f301e9b3dd208605614612828dc89b959c
Purvi Kanal <purvikanal@honeycomb.io>
2025-03-07 10:44:40 -0500

    very basic implementation of errors autoinstrumentation

--- plugins/web/opentelemetry-instrumentation-web-exception/src/instrumentation.ts
+++ plugins/web/opentelemetry-instrumentation-web-exception/src/instrumentation.ts
@@ -18,12 +18,68 @@ import {
   InstrumentationBase,
   InstrumentationConfig,
 } from '@opentelemetry/instrumentation';
+import {
+  ATTR_EXCEPTION_TYPE,
+  ATTR_EXCEPTION_MESSAGE,
+  ATTR_EXCEPTION_STACKTRACE,
+} from '@opentelemetry/semantic-conventions';
+// TODO: should events be imported or included as part of the instrumentation base class?
+import { events } from '@opentelemetry/api-events';
+import { SeverityNumber } from '@opentelemetry/api-logs';
+import { Attributes } from '@opentelemetry/api';
+
+export interface GlobalErrorsInstrumentationConfig
+  extends InstrumentationConfig {
+  /**
+   * A callback function for adding custom attributes to the span when an error is recorded.
+   *
+   * @param {Error} error - The error object that is being recorded.
+   * @returns {Attributes} - The attributes to add to the span.
+   */
+  applyCustomAttributes?: (error: Error) => Attributes;
+}
 
 export class WebExceptionInstrumentation extends InstrumentationBase<InstrumentationConfig> {
   constructor(config: InstrumentationConfig = {}) {
     super('@opentelemetry/instrumentation-web-exception', '0.0.1', config);
-    console.log('WebExceptionInstrumentation was constructed');
   }
 
   init() {}
+
+  onError = (event: ErrorEvent | PromiseRejectionEvent) => {
+    const error: Error | undefined =
+      'reason' in event ? event.reason : event.error;
+    if (error) {
+      const message = error.message;
+      const type = error.name;
+      const errorAttributes = {
+        [ATTR_EXCEPTION_TYPE]: type,
+        [ATTR_EXCEPTION_MESSAGE]: message,
+        [ATTR_EXCEPTION_STACKTRACE]: error.stack,
+      };
+
+      const eventLogger = events.getEventLogger(
+        this.instrumentationName,
+        this.instrumentationVersion
+      );
+
+      eventLogger.emit({
+        name: 'exception',
+        data: errorAttributes,
+        // TODO: add attributes from apply custom attributes function
+        severityNumber: SeverityNumber.ERROR,
+        // TODO: figure out timestamp
+      });
+    }
+  };
+
+  override disable(): void {
+    window.removeEventListener('error', this.onError);
+    window.removeEventListener('unhandledrejection', this.onError);
+  }
+
+  override enable(): void {
+    window.addEventListener('error', this.onError);
+    window.addEventListener('unhandledrejection', this.onError);
+  }
 }
