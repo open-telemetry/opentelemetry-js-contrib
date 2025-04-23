d2eaa31fb15121ec23f4a53894875a0e84783d14
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2025-04-23 14:03:28 -0700

    Format

--- .github/workflows/scripts/add-labels-to-reviewed-pr.sh
+++ .github/workflows/scripts/add-labels-to-reviewed-pr.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 #
 # Copyright The OpenTelemetry Authors
-# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Adds a "has:owner-approval" label to a PR if a reviewer who approved it is a component owner.
 
@@ -17,7 +28,6 @@ main () {
 
     # The latestReviews key returns the latest review for each reviewer cutting out any other reviews.
     JSON=$(gh pr view "${PR}" --json "files,author,latestReviews" | tr -dc '[:print:]' | sed -E 's/\\[a-z]//g')
-    AUTHOR=$(echo -n "${JSON}"| jq -r '.author.login')
     FILES=$(echo -n "${JSON}"| jq -r '.files[].path')
     LATEST_REVIEWS=$(echo -n "${JSON}" | jq -c '.latestReviews')
 
@@ -40,7 +50,6 @@ main () {
             if [[ -v PROCESSED_COMPONENTS["${COMPONENT}"] ]]; then
                 continue
             fi
-
             PROCESSED_COMPONENTS["${COMPONENT}"]=true
 
             # Check if updated file is owned by one of the reviewers"
