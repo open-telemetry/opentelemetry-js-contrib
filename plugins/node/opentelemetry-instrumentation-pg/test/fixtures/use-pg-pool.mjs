bb00805ae5f128382db9bea8a8dd019f0998476d
Marylia Gutierrez <maryliag@gmail.com>
2025-05-07 16:34:34 -0400

    Update plugins/node/opentelemetry-instrumentation-pg/test/fixtures/use-pg-pool.mjs
    Co-authored-by: Marc Pichler <marc.pichler@dynatrace.com>

--- plugins/node/opentelemetry-instrumentation-pg/test/fixtures/use-pg-pool.mjs
+++ plugins/node/opentelemetry-instrumentation-pg/test/fixtures/use-pg-pool.mjs
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-// Use postgres from an ES module:
-//    node --experimental-loader=@opentelemetry/instrumentation/hook.mjs use-pg.mjs
+// Use pg-pool from an ES module:
+//    node --experimental-loader=@opentelemetry/instrumentation/hook.mjs use-pg-pool.mjs
 
 import { trace } from '@opentelemetry/api';
 import { createTestNodeSdk } from '@opentelemetry/contrib-test-utils';
