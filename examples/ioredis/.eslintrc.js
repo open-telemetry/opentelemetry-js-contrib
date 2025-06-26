fd343ad7becb06c36aef2bae89c9ce3d8f4faf15
Trent Mick <trentm@gmail.com>
2025-06-26 10:59:23 -0700

    chore(examples): lint examples/ioredis using shared top-level eslint config
    Refs: https://github.com/open-telemetry/opentelemetry-js-contrib/issues/2891

--- examples/ioredis/.eslintrc.js
+++ examples/ioredis/.eslintrc.js
@@ -16,28 +16,11 @@
 
 'use strict';
 
-// Require tracer before any other modules
-require('./tracer');
-const Redis = require('ioredis');
+const baseConfig = require('../../eslint.config');
 
-const redis = new Redis();
-
-async function main() {
-  try {
-    await redis.set('test', 'data');
-  } catch (error) {
-    console.error(error);
-  }
-
-  // The process must live for at least the interval past any traces that
-  // must be exported, or some risk being lost if they are recorded after the
-  // last export.
-  console.log(
-    'Sleeping 5 seconds before shutdown to ensure all records are flushed.'
-  );
-  setTimeout(() => {
-    console.log('Completed.');
-  }, 5000);
-}
-
-main();
+module.exports = {
+  ...baseConfig,
+  env: {
+    node: true,
+  },
+};
