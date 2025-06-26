6bed39f2454872d50d1296f9407ddc85e5418e50
Trent Mick <trentm@gmail.com>
2025-06-26 10:52:53 -0700

    chore(examples): lint examples/grpc-census-prop using shared top-level eslint config
    Refs: https://github.com/open-telemetry/opentelemetry-js-contrib/issues/2891

--- examples/grpc-census-prop/.eslintrc.js
+++ examples/grpc-census-prop/.eslintrc.js
@@ -16,26 +16,11 @@
 
 'use strict';
 
-const tracing = require('@opencensus/nodejs');
-const { ConsoleExporter } = require('@opencensus/core');
+const baseConfig = require('../../eslint.config');
 
-const defaultBufferConfig = {
-  bufferSize: 1,
-  bufferTimeout: 2000,
-};
-
-/**
- * Return an OpenCensus tracer configured to use the gRPC plugin
- */
-module.exports = () => {
-  const { tracer } = tracing.start({
-    samplingRate: 1,
-    plugins: {
-      grpc: '@opencensus/instrumentation-grpc',
-    },
-  });
-
-  tracer.registerSpanEventListener(new ConsoleExporter(defaultBufferConfig));
-
-  return tracer;
+module.exports = {
+  ...baseConfig,
+  env: {
+    node: true,
+  },
 };
