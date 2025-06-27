12d3aa6dfd6f5b702bc0a4b9d480943f52c9a2be
Trent Mick <trentm@gmail.com>
2025-06-27 02:11:42 -0700

    chore(examples): lint examples/memcached using shared top-level eslint config (#2904)
    Refs: https://github.com/open-telemetry/opentelemetry-js-contrib/issues/2891

--- examples/memcached/.eslintrc.js
+++ examples/memcached/.eslintrc.js
@@ -16,25 +16,11 @@
 
 'use strict';
 
-require('./tracer')('example-resource');
-const Memcached = require('memcached');
-const assert = require('assert');
+const baseConfig = require('../../eslint.config');
 
-const KEY = '_KEY_';
-const VALUE = `RAND:${Math.random().toFixed(4)}`;
-const LT = 10;
-const client = new Memcached();
-
-client.set(KEY, VALUE, LT, err => {
-  assert.strictEqual(err, undefined);
-  client.get(KEY, (err, result) => {
-    assert.strictEqual(err, undefined);
-    assert.strictEqual(result, VALUE);
-    console.log(
-      'Sleeping 5 seconds before shutdown to ensure all records are flushed.'
-    );
-    setTimeout(() => {
-      console.log('Completed.');
-    }, 5000);
-  });
-});
+module.exports = {
+  ...baseConfig,
+  env: {
+    node: true,
+  },
+};
