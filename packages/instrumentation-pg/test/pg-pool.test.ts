a3ac2f2ad60433179a85202a4739082bdcfbe280
David Luna <david.luna@elastic.co>
2025-07-23 18:55:47 +0200

    chore: fix conflicts with main

--- packages/instrumentation-pg/test/pg-pool.test.ts
+++ packages/instrumentation-pg/test/pg-pool.test.ts
@@ -982,9 +982,8 @@ describe('pg semantic conventions env variable', () => {
   const provider = new BasicTracerProvider({
     spanProcessors: [new SimpleSpanProcessor(memoryExporter)],
   });
-  const testPostgres = process.env.RUN_POSTGRES_TESTS; // For CI: assumes local postgres db is already available
-  const testPostgresLocally = process.env.RUN_POSTGRES_TESTS_LOCAL; // For local: spins up local postgres db via docker
-  const shouldTest = testPostgres || testPostgresLocally; // Skips these tests if false (default)
+  const testPostgres = process.env.RUN_POSTGRES_TESTS;
+  const shouldTest = testPostgres;
   // Here we are `require`ing *before* the instrumentation is created below.
   // In *general* this is a potential instrumentation issue, but this works for
   // `pg-pool` instrumentation because it patches the `pgPool.prototype`
@@ -1003,21 +1002,11 @@ describe('pg semantic conventions env variable', () => {
     if (!shouldTest) {
       skip();
     }
-
-    if (testPostgresLocally) {
-      testUtils.startDocker('postgres');
-    }
   });
 
   beforeEach(() => {
     contextManager = new AsyncLocalStorageContextManager().enable();
     context.setGlobalContextManager(contextManager);
-  });
-
-  after(() => {
-    if (testPostgresLocally) {
-      testUtils.cleanUpDocker('postgres');
-    }
   });
 
   afterEach(() => {
