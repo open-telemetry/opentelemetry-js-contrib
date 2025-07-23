f9666102f9f32c80f0b5f2b35b160267453f2855
Trent Mick <trentm@gmail.com>
2025-07-23 13:22:29 -0700

    fix(winston-transport, instrumentation-winston): get the correct severityText and severityNumber with winston.format.colorize() is used
    Before this change the "log sending" feature of instr-winston would
    incorrectly include ANSI color codes in the `severityText` for emitted
    OTel LogRecords when the winston logger being instrumented was
    configured with the `colorize()` formatter, e.g.:
    
        const logger = winston.createLogger({
          format: winston.format.combine(
            winston.format.timestamp(),
            winston.format.colorize(),
            winston.format.simple()
          ),
          ...
    
    This is because the winston-transport.OpenTelemetryTransportV3 was using
    the Winston log info's "level" attribute... which was tweaked by
    `colorize()`. Winston log `info` objects have a `Symbol.for("info")`
    property which has the unchanged level name:
      https://github.com/winstonjs/winston#streams-objectmode-and-info-objects
    
    This commit *also* changes (fix) what `npm test` runs in the
    instrumentation-winston package. Before this, `npm test` was attempting
    to test against winston@2 and winston@3 -- presumably for better
    coverage. However, it ended up only ever testing with winston@2
    (`test-v1-v2` would install winston@2 and test, then `test-v3` would
    run the same test again with whatever current version is installed,
    which is now winston@2).
    
    The (IMO) correct answer here is:
    - `npm test` tests against the latest ver of the target module (winston@3)
    - `npm run test-all-versions` handles testing against other versions
    - coverage can take a back seat. (Aside: changes coming as part of
      https://github.com/open-telemetry/opentelemetry-js-contrib/pull/2866
      will, IIUC, provide coverage from test-all-versions runs.)

--- packages/winston-transport/src/utils.ts
+++ packages/winston-transport/src/utils.ts
@@ -60,19 +60,20 @@ function getSeverityNumber(level: string): SeverityNumber | undefined {
 }
 
 export function emitLogRecord(
-  record: Record<string, any>,
+  record: Record<string | symbol, any>,
   logger: Logger
 ): void {
   const { message, level, ...splat } = record;
   const attributes: LogAttributes = {};
+  const levelSym = record[Symbol.for('level')];
   for (const key in splat) {
     if (Object.prototype.hasOwnProperty.call(splat, key)) {
       attributes[key] = splat[key];
     }
   }
   const logRecord: LogRecord = {
-    severityNumber: getSeverityNumber(level),
-    severityText: level,
+    severityNumber: getSeverityNumber(levelSym),
+    severityText: levelSym,
     body: message,
     attributes: attributes,
   };
