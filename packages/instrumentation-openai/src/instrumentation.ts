c6ac4ac4b3b4e3447978c21d47b88b8a3a603b14
Anuraag Agrawal <anuraaga@gmail.com>
2025-07-31 11:08:21 +0900

    Comment

--- packages/instrumentation-openai/src/instrumentation.ts
+++ packages/instrumentation-openai/src/instrumentation.ts
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// avoids a dependency on @opentelemetry/core for hrTime utilities
 import { performance } from 'perf_hooks';
 
 import { context, SpanKind, SpanStatusCode, trace } from '@opentelemetry/api';
