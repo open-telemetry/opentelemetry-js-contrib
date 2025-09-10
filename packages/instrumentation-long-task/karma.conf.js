deaa17fe4dd2e28fc589a3a8d5f495472d49d43c
overbalance <overbalance@users.noreply.github.com>
2025-09-10 00:21:02 -0500

    fix(karma): load karma plugins from root package.json

--- packages/instrumentation-long-task/karma.conf.js
+++ packages/instrumentation-long-task/karma.conf.js
@@ -18,6 +18,15 @@ const karmaWebpackConfig = require('../../karma.webpack');
 const karmaBaseConfig = require('../../karma.base');
 
 module.exports = config => {
+  {
+    const plugins = (karmaBaseConfig.plugins = []);
+    const rootPackageJson = require('../../package.json');
+    const toAdd = Object.keys(rootPackageJson.devDependencies || {})
+      .filter(packageName => packageName.startsWith('karma-'))
+      .map(packageName => require(packageName));
+    plugins.push(...toAdd);
+  }
+
   config.set(
     Object.assign({}, karmaBaseConfig, {
       webpack: karmaWebpackConfig,
