7ff09b9749799c50e0540639d0467511eb28c03e
Trent Mick <trentm@gmail.com>
2025-09-15 13:01:38 -0700

    convert from eslint-plugin-license-header to eslint-plugin-yet-another-license-header to get support for a *pattern* of allowable Copyright sections
    Manually adjust instr-oracledb and resource-detector-gcp slightly for the defined Copyright format.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,51 @@
-/* eslint-disable license-header/header */
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
-import licenseHeaderPlugin from 'eslint-plugin-license-header';
 import nodePlugin from 'eslint-plugin-n';
+import yalhPlugin from 'eslint-plugin-yet-another-license-header';
 
-const license = [
-  '/*',
-  ' * Copyright The OpenTelemetry Authors',
-  ' *',
-  ' * Licensed under the Apache License, Version 2.0 (the "License");',
-  ' * you may not use this file except in compliance with the License.',
-  ' * You may obtain a copy of the License at',
-  ' *',
-  ' *      https://www.apache.org/licenses/LICENSE-2.0',
-  ' *',
-  ' * Unless required by applicable law or agreed to in writing, software',
-  ' * distributed under the License is distributed on an "AS IS" BASIS,',
-  ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
-  ' * See the License for the specific language governing permissions and',
-  ' * limitations under the License.',
-  ' */',
-];
+const defaultLicense = `
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+`;
+
+// This matches `defaultLicense`, but allows additional Copyright holders
+// either on the same line:
+//    Copyright The OpenTelemetry Authors, <someone>, <another someone>
+// and/or on subsequent lines:
+//    Copyright The OpenTelemetry Authors
+//    Copyright <someone>
+//    Copyright <another someone>
+const licensePattern = /^\/\*\n \* Copyright The OpenTelemetry Authors(, .+)*\n( \* Copyright .+\n)* \*\n \* Licensed under the Apache License, Version 2.0 \(the "License"\);\n \* you may not use this file except in compliance with the License.\n \* You may obtain a copy of the License at\n \*\n \* {6}https:\/\/www.apache.org\/licenses\/LICENSE-2.0\n \*\n \* Unless required by applicable law or agreed to in writing, software\n \* distributed under the License is distributed on an "AS IS" BASIS,\n \* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n \* See the License for the specific language governing permissions and\n \* limitations under the License.\n \*\/$/
 
 const baseConfig = tseslint.config(
   {
@@ -35,7 +59,7 @@ const baseConfig = tseslint.config(
   {
     files: ['**/*.{js,ts,mjs}', '*.{js,ts,mjs}'],
     plugins: {
-      'license-header': licenseHeaderPlugin,
+      'yet-another-license-header': yalhPlugin,
       node: nodePlugin,
     },
     extends: [
@@ -58,7 +82,10 @@ const baseConfig = tseslint.config(
       quotes: ['error', 'single', { avoidEscape: true }],
       eqeqeq: ['error', 'smart'],
       'prefer-rest-params': 'off',
-      'license-header/header': ['error', license],
+      'yet-another-license-header/header': ['error', {
+        header: defaultLicense,
+        allowedHeaderPatterns: [licensePattern],
+      }],
 
       // new rules
       'no-unused-vars': 'warn',
