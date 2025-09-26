f7b3367bf48d76ea7b2f1ccfb5d4f6fa97c69eae
overbalance <overbalance@users.noreply.github.com>
2025-09-25 23:21:11 -0500

    fix some non-header lints

--- scripts/version-update.js
+++ scripts/version-update.js
@@ -1,19 +1,3 @@
-/*!
- * Copyright The OpenTelemetry Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
@@ -44,6 +28,6 @@ export const PACKAGE_VERSION = '${pjson.version}';
 export const PACKAGE_NAME = '${pjson.name}';
 `;
 
-const fileUrl = path.join(appRoot, 'src', 'version.ts')
+const fileUrl = path.join(appRoot, 'src', 'version.ts');
 
 fs.writeFileSync(fileUrl, content);
