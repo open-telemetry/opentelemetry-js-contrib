2c33d6486fa24c0da0c3bc255e87d2ed664f5343
wpessers <warre.pessers@gmail.com>
2026-04-01 22:54:24 +0200

    refactor: invert if condition

--- packages/instrumentation-aws-lambda/src/instrumentation.ts
+++ packages/instrumentation-aws-lambda/src/instrumentation.ts
@@ -608,9 +608,9 @@ export class AwsLambdaInstrumentation extends InstrumentationBase<AwsLambdaInstr
       );
       const spanContext = trace.getSpanContext(propagatedContext);
 
-      if (!spanContext) continue;
-
-      links.push({ context: spanContext });
+      if (spanContext) {
+        links.push({ context: spanContext })
+      };
     }
     return links;
   }
