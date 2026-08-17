604935075066c2825222afc5cafb7892a047b155
Arya <arya.mohanan@ibm.com>
2026-08-17 16:45:02 +0530

    feat(instrumentation-ioredis): support ioredis @v6 (#3666)

--- packages/instrumentation-ioredis/src/instrumentation.ts
+++ packages/instrumentation-ioredis/src/instrumentation.ts
@@ -48,7 +48,7 @@ export class IORedisInstrumentation extends InstrumentationBase<IORedisInstrumen
     return [
       new InstrumentationNodeModuleDefinition(
         'ioredis',
-        ['>=2.0.0 <6'],
+        ['>=2.0.0 <7'],
         (module, moduleVersion?: string) => {
           const moduleExports =
             module[Symbol.toStringTag] === 'Module'
